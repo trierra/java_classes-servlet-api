a6e91ac30bc132c3e7ddcf8f3c1f09e1597b5a2f
trierra <trierra.dev@gmail.com>
2015-08-16 17:11:35 -0700

    according to last video

---
+++ src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,47 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath() + "/login.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Map<String, String> messages = new HashMap<String, String>();
+
+        request.setAttribute("messageMap", messages);
+
+        String login = request.getParameter("loginParam");
+
+        if (login == null) {
+            messages.put("login", "Please enter login");
+        }
+
+        String password = request.getParameter("passwd");
+
+        if (password == null) {
+            messages.put("passwd", "Please enter password");
+        }
+
+        if (messages.isEmpty()) {
+            messages.put("userLogin", login);
+            messages.put("success", String.format("Hello %s !, your login is %s and password is %s", login, login, password));
+        }
+
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+
+    }
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+<form action="/myapp/login" method="post">
+
+  Login: <input type="text" name="loginParam"/>
+  Password: <input type="password" name="passwd"/>
+  <input type="submit" value="Submit"/>
+
+</form>
+
+</body>
+</html>
